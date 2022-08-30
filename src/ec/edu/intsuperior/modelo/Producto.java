f922ee704aaf424d52f7f7c3669621e540a1302d
User <User@DESKTOP-0A5SAM6>
2022-08-30 09:22:58 -0500

    Proyecto Final Desiree Bajaña 2A

---
+++ src/ec/edu/intsuperior/modelo/Producto.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.modelo;
+
+/**
+ *
+ * @author User
+ */
+public class Producto {
+    private String precioUnitario;
+    private String sabor;
+    private String porcentaje;
+    
+    public Producto(){
+        
+    }
+
+    
+    
+    
+}
